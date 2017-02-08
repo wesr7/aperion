b14b4aa0a4c8f74b820fb51f2ce0f520a46eb26f
wesr7 <jwesleyrodgers@gmail.com>
2017-02-07 23:40:40 -0800

    removed font directory from repo

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
 $(document).ready(function(){
-    $('.parallax').parallax();
+    if ($('.home-bg').length) {
+        $('.parallax').parallax();
+    }
+    if ($('.platform-bg').length) {
+        $('.slider-for').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: true,
+            fade: true,
+            asNavFor: '.slider-nav'
+        });
+        $('.slider-nav').slick({
+            slidesToShow: 7,
+            slidesToScroll: 1,
+            asNavFor: '.slider-for',
+            centerMode: true,
+            focusOnSelect: true
+        });
+    }
+
 });
