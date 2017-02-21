f61746857a6946b0c9aff9a9b8d40f310cd053fb
wesr7 <jwesleyrodgers@gmail.com>
2017-02-20 16:49:44 -0800

    messaging and voice pages 90%

--- js/main.js
+++ js/main.js
@@ -1,7 +1,29 @@
 $(document).ready(function(){
     const voiceFeatures = {
-        'networkInterconnect' : 'Private IP connection to guarantee quality of service and eliminate public network risks.'
+        'network-interconnect' : 'Private IP connection to guarantee quality of service and eliminate public network risks.',
+        'secure-trunking' : 'Encrypt communcications between your application and the Apeiron platform using SSL & IPSEC VPNs over the public net.',
+        'call-recording' : 'Turn call recording on and off and retrieve recorded files via API or GUI',
+        'fax-and-voicemail' : 'A la carte voicemail and fax features available for use with DIDs and TFNs.',
+        'hd-voice-and-transcoding' : 'Super sweet RTP treatments',
+        'call-forwarding' : 'On-demand call forwarding features for managing your numbers.',
+        'sms' : 'SMS enable any DID or Toll Free number to receive text messages',
+        'e911-cnam-and-listings' : 'Full Featured DID and TFNs suitable for any business use.',
+        'iot-endpoint-features' : 'Platform features that allow you to offload session and edge security functions.',
+        'full-featured-api' : 'To provision, use and mangage all of our voice services and features'
     }
+    const messagingFeatures = {
+        'network-interconnect' : 'Security and Compliance features for platforms using private IP.',
+        'inbound-delivery' : 'Receive SMS by Webhook, Email or SMS',
+        'rest-api' : 'Receive SMS by Webhook, Email or SMS',
+        'sdk-support' : 'Developer portal with SDK support for integration to your application.',
+        'longcodes' : 'Local phone number and Toll Free phone number support',
+        'shortcodes' : 'High volume and campaign specific SMS management',
+        'compliance-features' : 'No-send list based on SMS unsubscribe/stop responses',
+        'list-management' : 'Load and manage user lists in Apeiron dashboard',
+        'campaign-management' : 'Bulk send operations using Lists',
+        'auto-responders' : 'Easy to use auto-response rules for inbound messages.'
+    }
+
     if ($('.home-bg').length) {
         $('.parallax').parallax();
     }
@@ -21,27 +43,23 @@ $(document).ready(function(){
             focusOnSelect: true
         });
     }
-    // let columns = $('.')
-    // function colHeight(col) {
-    //     col.each(function(){
-    //         console.log(col);
-    //     });
-    // }
-
-    // colHeight()
-
-    const toggleContent = function(element, obj) {
-        $(element).click(function(){
-            let name = $(this).data('name');
-            const paragraph = $(this).next('p');
+    const toggleContent = function(elements, obj) {
+        $(elements).click(function(e){
+            const info = $(this).data('name');
+            const paragraph = $(this);
             paragraph.toggleClass('up');
             if (paragraph.is(':empty')) {
-                paragraph.append(obj[name]);
+                paragraph.append(obj[info]);
             } else {
                 paragraph.empty();
             }
         });
     }
-    toggleContent('h4', voiceFeatures)
+    if ($('.voice').length) {
+        toggleContent($('.features p'), voiceFeatures)
+    } else if ($('.messaging').length) {
+        toggleContent($('.features p'), messagingFeatures)
+    }
+
 
 });
