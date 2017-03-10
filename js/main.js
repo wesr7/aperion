eeec5d8f2651ac92c5beff0cc8bba7898aa0296c
wesr7 <jwesleyrodgers@gmail.com>
2017-03-10 00:02:12 -0800

    fixing slider bugs and others

--- js/main.js
+++ js/main.js
@@ -1,48 +1,68 @@
 $(document).ready(function(){
     const voiceFeatures = {
-        'network-interconnect' : 'Private IP connection to guarantee quality of service and eliminate public network risks',
-        'secure-trunking' : 'Encrypt communcications between your application and the Apeiron platform using SSL & IPSEC VPNs over the public net',
+        'network-interconnect' : 'Private IP options to guarantee QoS and eliminate public network risks.',
+        'secure-trunking' : 'Encrypted voice options to secure communications between your application, the Apeiron platform, and/or your end users.  Includes PCI compliant sip trunking.',
         'call-recording' : 'Turn call recording on and off and retrieve recorded files via API or GUI',
-        'fax-and-voicemail' : 'A la carte voicemail and fax features available for use with DIDs and TFNs.',
-        'hd-voice-and-transcoding' : 'Super sweet RTP treatments',
-        'call-forwarding' : 'On-demand call forwarding features for managing your numbers',
-        'sms' : 'SMS enable any DID or Toll Free number to receive text messages',
-        'e911-cnam-and-listings' : 'Full Featured DID and TFNs suitable for any business use',
-        'iot-endpoint-features' : 'Platform features that allow you to offload session and edge security functions',
-        'full-featured-api' : 'To provision, use and mangage all of our voice services and features'
+        'fax-and-voicemail' : 'Voicemail and Fax functions available for use with DIDs and TFNs and supported by our REST API.',
+        'hd-voice-and-transcoding' : 'High quality codecs and useful RTP features available on SIP trunks and as a la carte functions.',
+        'call-forwarding' : 'On-demand call fowarding and disaster recovery features for DIDs, TFNs & SIP trunks with sim-ring and hunt configurations.',
+        'sms' : 'SMS enable any Local Number (DID), Toll Free Number or Short Code to send and receive text messages.',
+        'e911-cnam-and-listings' : 'Comprehensive DID & TFN feature set capable of supporting any business requirement.',
+        'iot-endpoint-features' : 'Endpoint services that allow you to offload registration, security and management functions for IOT clients at scale.',
+        'full-featured-api' : 'Access real-time config, performance and billing information about your services via a REST API.  Our API coverage is legit.'
     }
     const messagingFeatures = {
-        'network-interconnect' : 'Security and Compliance features for platforms using private IP',
-        'inbound-delivery' : 'Receive SMS by Webhook, Email or SMS',
-        'rest-api' : 'Receive SMS by Webhook, Email or SMS',
-        'sdk-support' : 'Developer portal with SDK support for integration to your application',
-        'longcodes' : 'Local phone number and Toll Free phone number support',
-        'shortcodes' : 'High volume and campaign specific SMS management',
-        'compliance-features' : 'No-send list based on SMS unsubscribe/stop responses',
-        'list-management' : 'Load and manage user lists in Apeiron dashboard',
-        'campaign-management' : 'Bulk send operations using Lists',
-        'auto-responders' : 'Easy to use auto-response rules for inbound messages'
+        'network-interconnect' : 'Private IP interconnection to any US address.  Now offering Public Cloud direct connect.',
+        'inbound-delivery' : 'Forward inbound SMS messages as they are received via webhook, email or SMS in addition to standard REST API options.',
+        'rest-api' : 'Full REST API support for SMS send and receive operations as well as advanced SMS platform features.',
+        'sdk-support' : 'Developer portal with SDK support for integrating Apeiron functions with your application.',
+        'longcodes' : 'P2P Messaging - Receive SMS messages sent to Local Numbers (DIDs) and Toll Free Numbers (TFNs).',
+        'shortcodes' : 'A2P & A2A Messaging - Send/Receive using codes optimized for high volume SMS operations.',
+        'compliance-features' : 'Manage SMS compliance with associated logging, reporting and notifications support.',
+        'list-management' : 'Load and manage user lists with template and list variable features.  Load data via the User Dashboard, csv upload or REST API.',
+        'campaign-management' : 'Schedule and execute bulk SMS send operations using Lists.',
+        'auto-responders' : 'Program auto-response rules applied per number that auto-reply to inbound messages.'
     }
+    if ($('.platform').length) {
+        if ($(window).width() > 780) {
+            $('.slider-for').slick({
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: true,
+                fade: true,
+                asNavFor: '.slider-nav'
+            });
+            $('.slider-nav').slick({
+                slidesToShow: 7,
+                slidesToScroll: 1,
+                asNavFor: '.slider-for',
+                centerMode: false,
+                focusOnSelect: true,
+                arrows: false
+            });
 
-    if ($('.platform').length > 0) {
-        $('.slider-for').slick({
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            arrows: true,
-            fade: true,
-            asNavFor: '.slider-nav'
-        });
-        $('.slider-nav').slick({
-            slidesToShow: 7,
-            slidesToScroll: 1,
-            asNavFor: '.slider-for',
-            centerMode: false,
-            focusOnSelect: true,
-            arrows: false
-        });
+        } else if ($(window).width() < 780) {
+            $('.slider-for').slick({
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: true,
+                fade: true,
+                asNavFor: '.slider-nav'
+            });
+            $('.slider-nav').slick({
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                asNavFor: '.slider-for',
+                centerMode: false,
+                focusOnSelect: true,
+                arrows: false,
+            });
+            $('.slider-nav.slick-initialized.slick-slider').attr('style', 'display: none');
+            $('.slider-nav .slick-slide.slick-cloned').remove();
+        }
         const trackWidth = $('.slider-for').width();
         $('.slick-list').attr('style', 'padding: 0 5px');
-        $('.slick-track').attr('style', 'width: ' + $('.slider-for').width() + 'px;');
+        $('.slick-track').attr('style', 'width: ' + trackWidth + 'px;');
         $('.slider-nav .slick-slide').each(function(){
             $(this).attr('style', 'width: ' + ((trackWidth / 7) - 20) + 'px');
         });
@@ -60,7 +80,7 @@ $(document).ready(function(){
     if ($('.pane-one').length && $(window).width() > 780){
         equalHeight($('.pane-three .col'), $('.pane-three p'));
     }
-    if ($('.page-rapper').length) {
+    if ($('.page-rapper').length && $(window).width() > 780) {
         equalHeight($('.features .col.s6'), $('.features i'));
     }
 
@@ -84,21 +104,27 @@ $(document).ready(function(){
     } else if ($('.messaging').length > 0) {
         toggleContent($('.features a'), messagingFeatures)
     }
-    $('a.right').click(function(e){
+    $('a#burger_menu').click(function(e){
         e.preventDefault();
         $(this).css('display', 'none');
+        $('body').css('overflow', 'hidden');
         $('div.hide').removeClass('hide').addClass('menu');
     });
     $('a#menu_burger, .menu_list > li > a').click(function(e){
         $('div.menu').removeClass('menu').addClass('hide');
+        $('body').css('overflow', 'initial');
         $('a.right').css('display', 'block');
     });
 });
 
+
 $(window).scroll(function(){
     if ($(window).scrollTop() > 90) {
-        $('nav').addClass('navDown')
+        $('nav#main').addClass('navDown')
     } else if ($(window).scrollTop() < 90) {
-       $('nav').removeClass('navDown')
+        $('nav#main').removeClass('navDown')
     }
 });
+
+
+
